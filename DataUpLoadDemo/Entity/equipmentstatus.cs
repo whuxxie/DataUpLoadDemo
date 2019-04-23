a33baf1313d5613816a8eda24250f9b5fe14231b
xxiew <xx_880729@163.com>
2019-04-23 16:42:06 +0800

    增加设备状态

---
+++ DataUpLoadDemo/Entity/equipmentstatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataUpLoadDemo.Entity
+{
+    public class equipmentstatus
+    {
+
+        public string companyid { set; get; }
+        public string equipmentcode { set; get; }
+        public string datetime { set; get; }
+        public string tagstate { set; get; }
+    }
+}
--- DataUpLoadDemo/Program.cs
+++ DataUpLoadDemo/Program.cs
@@ -22,9 +22,14 @@ namespace DataUpLoadDemo
             data data2 = new data() { tagid = "S02-Rtd", tagname = "XXX", tagdata = "13.21", tagstate = "00" };
             data data3 = new data() { tagid = "S03-Rtd", tagname = "XXX", tagdata = "112.21", tagstate = "00" };
             data data4 = new data() { tagid = "S04-Rtd", tagname = "XXX", tagdata = "133.21", tagstate = "00" };
-            monitordata monitordatamodel = new monitordata() { companyid = companyid, equipmentcode = "5FF45AB7-1E72-4381-A0A6-26erd0F54F", datetime = "2019-04-19 12:00:00", surveytypecode = "EXH_Real",datas=new List<data>() { data1, data2, data3, data4 } };
+            monitordata monitordatamodel = new monitordata() { companyid = companyid, equipmentcode = "F4F6F436-84AA-429A-8271-24CF4BE4FE41", datetime = "2019-04-23 12:00:00", surveytypecode = "EXH_Real",datas=new List<data>() { data1, data2, data3, data4 } };
             var datainfo = WebApiHelper.Post<HttpResponseMsg>("http://localhost:1207/api/DataManipulate/DataUpLoad", JsonConvert.SerializeObject(monitordatamodel), companyid);
             Console.WriteLine(datainfo.ToString());
+            Console.WriteLine("--------------------------------");
+
+            equipmentstatus edata = new equipmentstatus() { companyid = companyid, equipmentcode = "F4F6F436-84AA-429A-8271-24CF4BE4FE41", datetime = "2019-04-23 12:00:00", tagstate = "02" };
+            var info= WebApiHelper.Post<HttpResponseMsg>("http://localhost:1207/api/DataManipulate/EquipmentStatus", JsonConvert.SerializeObject(edata), companyid);
+            Console.WriteLine(info.ToString());
             Console.Read();
         }
     }
